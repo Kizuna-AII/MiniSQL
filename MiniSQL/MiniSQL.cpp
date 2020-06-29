61fcd4e35c056492260636202c32cde0008201cb
Kizuna-AII <zty61211@gmail.com>
2020-06-29 17:47:26 +0800

    CLI parameter added

--- MiniSQL/MiniSQL.cpp
+++ MiniSQL/MiniSQL.cpp
@@ -10,13 +10,15 @@
 using namespace std;
 using namespace API;
 
-#define USEFILECOMMAND (false)
+#define USEFILECOMMAND (true)
 #define USEFILELOG (false)
 #define WORKPATH (std::string("../test/"))
-#define COMMANDFILENAME (std::string("command.txt"))
 #define LOGFILENAME (std::string("log.txt"))
 
-class miniSQL {
+std::string COMMANDFILENAME("command.txt");
+
+class miniSQL
+{
 private:
 	map<string, int>parsemap;
 	Api* api;
@@ -40,7 +42,7 @@ private:
 		fin >> skipws;
 		string comm;
 		fin >> comm;
- 		map<string, int>::iterator iter = parsemap.find(comm);
+		map<string, int>::iterator iter = parsemap.find(comm);
 		if (iter == parsemap.end()) {//指令错误
 			return -1;
 		}
@@ -62,132 +64,29 @@ public:
 			int x = Parse_once(fin);
 			try {
 				switch (x) {
-					case 1: {//create
-						switch (Parse_once(fin)) {
-							case 2: {//create table
-								string tName;
-								inter->GetString(fin, tName);
-								vector<Common::Attribute> attributes = inter->GetAttributes(fin);
-								inter->PeekEnd(fin);
-								api->CreateTable(tName, attributes);
-								break;
-							}
-							case 3: {//create index
-								string indexName, tableName,attriName;
-								inter->GetString(fin, indexName);//获取index名
-								inter->GetString(fin, tableName);
-								if (tableName == "on") {//则读取table名
-									inter->GetString(fin, tableName);
-									inter->GetString(fin, attriName);
-								}
-								else {
-									throw(wrong_command_error("no table name"));
-								}
-								api->CreateIndex(indexName, tableName, attriName);
-								inter->ClearCommand(fin);//跳过末尾';'
-								break;
-							}
-							default: {
-								inter->ClearCommand(fin);//抛弃整个错误语句
-								throw(wrong_command_error(""));
-								break;
-							}
-						}
-						break;
-					}
-					case 4: {//drop
-						switch (Parse_once(fin)) {
-							case 2: {//drop table
-								string tableName, temp;
-								inter->GetString(fin, tableName);//获取table名
-								api->DropTable(tableName);
-								inter->ClearCommand(fin);//跳过末尾';'
-								break;
-							}
-							case 3: {//drop index
-								string indexName,temp;
-								inter->GetString(fin, indexName);//获取index名
-								inter->GetString(fin,temp);
-								if (temp == "on") {//如果指定table，则读取table名
-									inter->GetString(fin, temp);
-								}
-								else {
-									temp = "";
-								}
-								api->DropIndex(indexName,temp);
-								inter->ClearCommand(fin);//跳过末尾';'
-								break;
-							}
-							default: {
-								inter->ClearCommand(fin);//抛弃整个错误语句
-								throw(wrong_command_error(""));
-								break;
-							}
-						}
-						break;
-					}
-					case 5: {//select
-						string temp,tableName;
-						vector<Common::Compares> conditions;
-						inter->GetString(fin, temp);
-						if(temp!="*")throw(wrong_command_error(""));
-						inter->GetString(fin, temp);
-						if (temp != "from")throw(wrong_command_error(""));
-						inter->GetString(fin, tableName);
-						if (!inter->PeekEnd(fin)) {
-							inter->GetString(fin, temp);
-							if(temp!="where")throw(wrong_command_error(""));
-							conditions = inter->GetConditions(fin);//获取选择条件
-							api->Select(tableName, &conditions);
-						}
-						else {
-							api->Select(tableName, NULL);//无条件选择
-						}
-						api->OutPutResult(tableName, fout);
-						break;
-					}
-					case 6: {//insert
-						string temp, tableName;
-						inter->GetString(fin, temp);
-						if (temp != "into")throw(wrong_command_error(""));
-						inter->GetString(fin, tableName);//读取表名
-						inter->GetString(fin, temp);
-						if (temp != "values")throw(wrong_command_error(""));
-						Common::Tuple tuple = inter->GetTuple(fin,tableName);//读取values
+				case 1: {//create
+					switch (Parse_once(fin)) {
+					case 2: {//create table
+						string tName;
+						inter->GetString(fin, tName);
+						vector<Common::Attribute> attributes = inter->GetAttributes(fin);
 						inter->PeekEnd(fin);
-						api->Insert(tuple, tableName);//insert
+						api->CreateTable(tName, attributes);
 						break;
 					}
-					case 7: {//delete
-						string temp, tableName;
-						vector<Common::Compares> conditions;
-						inter->GetString(fin, temp);
-						if (temp != "from")throw(wrong_command_error(""));
+					case 3: {//create index
+						string indexName, tableName, attriName;
+						inter->GetString(fin, indexName);//获取index名
 						inter->GetString(fin, tableName);
-						if (!inter->PeekEnd(fin)) {
-							inter->GetString(fin, temp);
-							if (temp != "where")throw(wrong_command_error(""));
-							conditions = inter->GetConditions(fin);//获取删除条件
-							api->Delete(tableName, &conditions);
+						if (tableName == "on") {//则读取table名
+							inter->GetString(fin, tableName);
+							inter->GetString(fin, attriName);
 						}
 						else {
-							api->Delete(tableName, NULL);//无条件删除
+							throw(wrong_command_error("no table name"));
 						}
-						break;
-					}
-					case 8: {//quit
-						flag = 0;
-						break;
-					}
-					case 9: {//exec
-						char fname[30];
-						fin.getline(fname, 30, ';');
-						ifstream tmpin(fname);//打开对应文件
-						Run(tmpin, fout, "exec fin");//递归处理文件读取
-						break;
-					}
-					case 10: {
-						fin.clear();
+						api->CreateIndex(indexName, tableName, attriName);
+						inter->ClearCommand(fin);//跳过末尾';'
 						break;
 					}
 					default: {
@@ -195,6 +94,109 @@ public:
 						throw(wrong_command_error(""));
 						break;
 					}
+					}
+					break;
+				}
+				case 4: {//drop
+					switch (Parse_once(fin)) {
+					case 2: {//drop table
+						string tableName, temp;
+						inter->GetString(fin, tableName);//获取table名
+						api->DropTable(tableName);
+						inter->ClearCommand(fin);//跳过末尾';'
+						break;
+					}
+					case 3: {//drop index
+						string indexName, temp;
+						inter->GetString(fin, indexName);//获取index名
+						inter->GetString(fin, temp);
+						if (temp == "on") {//如果指定table，则读取table名
+							inter->GetString(fin, temp);
+						}
+						else {
+							temp = "";
+						}
+						api->DropIndex(indexName, temp);
+						inter->ClearCommand(fin);//跳过末尾';'
+						break;
+					}
+					default: {
+						inter->ClearCommand(fin);//抛弃整个错误语句
+						throw(wrong_command_error(""));
+						break;
+					}
+					}
+					break;
+				}
+				case 5: {//select
+					string temp, tableName;
+					vector<Common::Compares> conditions;
+					inter->GetString(fin, temp);
+					if (temp != "*")throw(wrong_command_error(""));
+					inter->GetString(fin, temp);
+					if (temp != "from")throw(wrong_command_error(""));
+					inter->GetString(fin, tableName);
+					if (!inter->PeekEnd(fin)) {
+						inter->GetString(fin, temp);
+						if (temp != "where")throw(wrong_command_error(""));
+						conditions = inter->GetConditions(fin);//获取选择条件
+						api->Select(tableName, &conditions);
+					}
+					else {
+						api->Select(tableName, NULL);//无条件选择
+					}
+					api->OutPutResult(tableName, fout);
+					break;
+				}
+				case 6: {//insert
+					string temp, tableName;
+					inter->GetString(fin, temp);
+					if (temp != "into")throw(wrong_command_error(""));
+					inter->GetString(fin, tableName);//读取表名
+					inter->GetString(fin, temp);
+					if (temp != "values")throw(wrong_command_error(""));
+					Common::Tuple tuple = inter->GetTuple(fin, tableName);//读取values
+					inter->PeekEnd(fin);
+					api->Insert(tuple, tableName);//insert
+					break;
+				}
+				case 7: {//delete
+					string temp, tableName;
+					vector<Common::Compares> conditions;
+					inter->GetString(fin, temp);
+					if (temp != "from")throw(wrong_command_error(""));
+					inter->GetString(fin, tableName);
+					if (!inter->PeekEnd(fin)) {
+						inter->GetString(fin, temp);
+						if (temp != "where")throw(wrong_command_error(""));
+						conditions = inter->GetConditions(fin);//获取删除条件
+						api->Delete(tableName, &conditions);
+					}
+					else {
+						api->Delete(tableName, NULL);//无条件删除
+					}
+					break;
+				}
+				case 8: {//quit
+					flag = 0;
+					break;
+				}
+				case 9: {//exec
+					char fname[30];
+					fin.getline(fname, 30, ';');
+					ifstream tmpin(fname);//打开对应文件
+					Run(tmpin, fout, "exec fin");//递归处理文件读取
+					break;
+				}
+				case 10: {
+					fin.clear();
+					break;
+				}
+				default: {
+					inter->ClearCommand(fin);//抛弃整个错误语句
+					throw(wrong_command_error(""));
+					break;
+				}
 				}
 			}
 			catch (mexception &ex) {
@@ -207,8 +209,13 @@ public:
 	}
 };
 
-int main() {
+
+int main(int argc, char **argv) {
 	/// initialize I/O
+	if (argc >= 2) // 指定Command Filename
+	{
+		COMMANDFILENAME = argv[1];
+	}
 	FILE * tempStream;
 	std::ios::sync_with_stdio(false);
 	if (USEFILECOMMAND)
